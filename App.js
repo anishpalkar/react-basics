173e3764e7929935eeb681721d37e9065c515206
Anish Palkar <anish.palkar@gmail.com>
2024-05-14 18:17:46 +0530

    npm setup + parcel integration

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const heading = React.createElement(
     "h1",
     {id:"heading"},
-    "Hello World from React!"
+    "Hello World from React!!"
 );
 
 console.log(heading); // this is an object which is created by React.createElement.
