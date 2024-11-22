2b241b1b08c126cdd536ce4fd571dfadd53560a0
Anish Palkar <anish.palkar@gmail.com>
2024-11-23 01:25:18 +0530

    react-router-dom : Outlet compoent loading children of a path & Link component to navigate to a route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
@@ -14,15 +14,33 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+
+      <Outlet />
+      {/*
+      Outlet will take care of loading the correct children of AppLayout. Outlet gets replaced by the Children Component according to the path in URL.
+       if path = /
       <Body />
+
+      if path = /about 
+      <About />
+      
+      if path = /contact
+      <Contact /> */}
     </div>
   );
 };
 
 const appRouter = createBrowserRouter([
-  { path: "/", element: <AppLayout />, errorElement: <Error /> },
-  { path: "/about", element: <About /> },
-  { path: "/contact", element: <Contact /> },
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Body /> },
+      { path: "/about", element: <About /> },
+      { path: "/contact", element: <Contact /> },
+    ],
+    errorElement: <Error />,
+  },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
