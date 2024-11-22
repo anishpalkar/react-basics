92fd3594b3c10e5b07e039d7186d37284d5f7edb
Anish Palkar <anish.palkar@gmail.com>
2024-11-23 01:06:58 +0530

    react-router-dom basic

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 // what should we give in path ./components/Header or ./components/Header.jsx? It doesn't matter, react treats it as a js file only
 
 // JSX Element => Babel transpiles => ReactElement => ReactElement(JS Object) => HTMLELement(render)
@@ -15,6 +19,12 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  { path: "/", element: <AppLayout />, errorElement: <Error /> },
+  { path: "/about", element: <About /> },
+  { path: "/contact", element: <Contact /> },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
--- src/components/Body.js
+++ src/components/Body.js
@@ -22,9 +22,9 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.2380683&lng=72.8522512&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-    console.log(
-      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    );
+    // console.log(
+    //   json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    // );
     setRestaurantToDisplay(
       json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
