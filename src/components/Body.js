5957d8bdefe3b6fc163ef721999f2582a5a4ae7d
Anish Palkar <anish.palkar@gmail.com>
2024-11-22 17:12:40 +0530

    useState, useEffect, Shimmer UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,26 +1,79 @@
 import RestaurantCard from "./RestuarantCard";
 import RES_DATA from "../utils/mockdata";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [restaurantToDisplay, setRestaurantToDisplay] = useState(RES_DATA);
-  return (
+  //useState is used to create "local state variables" inside functional components
+  //Whenever state variables update, react triggers a reconciliation cycle(re-renders the component)
+  const [restaurantToDisplay, setRestaurantToDisplay] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filteredRestaurantToDisplay, setFilteredRestaurantToDisplay] =
+    useState([]);
+  //on change in state variable React re-renders the whole component in Virtual DOM with constructing a new tree with updates, it will compare both the old and newly constructed tree (Diff algo) and then it will only update "changed part"(diffed) in actual DOM.
+
+  //below useEffect callback is called after the component renders itself first time (see Header.js)
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.2380683&lng=72.8522512&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setRestaurantToDisplay(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurantToDisplay(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  return restaurantToDisplay.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              console.log(searchText);
+              const filteredList = restaurantToDisplay.filter((restaurant) =>
+                restaurant?.info?.name
+                  ?.toLowerCase()
+                  .includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurantToDisplay(filteredList);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
             const filteredList = restaurantToDisplay.filter(
-              (restaurant) => restaurant.info.avgRating > 4
+              (restaurant) => restaurant?.info?.avgRating > 4.5
             );
-            setRestaurantToDisplay(filteredList);
+            setFilteredRestaurantToDisplay(filteredList);
           }}
         >
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {restaurantToDisplay.map((restaurant) => {
+        {filteredRestaurantToDisplay.map((restaurant) => {
           return (
             <RestaurantCard key={restaurant.info.id} resData={restaurant} />
           );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,22 @@
 // Name of file shoule be same as component name => convention followed
 // we can use .js or .jsx extensions even .tsx, so what is a good way? .js can be followed, .jsx also can be used it hardly matters don't overthink.
 
+import { useEffect, useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
+
+  //-------- useEffect() --------------
+  //If no dependency array => useEffect is called on every render cycle (initial + after every re-render)
+  //useEffect(() => console.log("useEffect called"));
+
+  //If dependency array is empty = [] => useEffect is called only on initial render i.e. only once when the component completes first render.
+  //useEffect(() => console.log("useEffect called"), []);
+
+  //If dependency array is [btnName] => useEffect is called on inital render + everytime btnName is updated
+  useEffect(() => console.log("useEffect called"), [btnName]);
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -15,6 +28,18 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <li>
+            <button
+              onClick={() => {
+                btnName === "Login"
+                  ? setBtnName("Logout")
+                  : setBtnName("Login");
+              }}
+              className="login-btn"
+            >
+              {btnName}
+            </button>
+          </li>
         </ul>
       </div>
     </div>
